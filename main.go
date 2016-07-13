f6334b365770433842f71695cbaf73fd6af9b160
Prateek Srivastava <f2prateek@gmail.com>
2016-07-13 03:53:44 -0600

    Dynamically redirect to CDN or Tracking API

--- main.go
+++ main.go
@@ -8,15 +8,51 @@ import (
 	"net/http/httptest"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 )
 
-// patchHost sets the host of the request to the host of of the destination URL.
-// See http://blog.semanticart.com/blog/2013/11/11/a-proper-api-proxy-written-in-go/.
-func patchHost(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.Host = r.URL.Host
-		handler.ServeHTTP(w, r)
-	})
+// singleJoiningSlash is copied from httputil.singleJoiningSlash method.
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+// NewSegmentReverseProxy is adapted from the httputil.NewSingleHostReverseProxy
+// method, modified to dynamically redirect to different servers (CDN or Tracking API)
+// based on the incoming request, and sets the host of the request to the host of of
+// the destination URL.
+func NewSegmentReverseProxy(cdn *url.URL, trackingAPI *url.URL) *httputil.ReverseProxy {
+	director := func(req *http.Request) {
+		// Figure out which server to redirect to based on the incoming request.
+		var target *url.URL
+		if strings.HasPrefix(req.URL.String(), "/v1/projects") {
+			target = cdn
+		} else {
+			target = trackingAPI
+		}
+
+		targetQuery := target.RawQuery
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+
+		// patchHost sets the host of the request to the host of of the destination URL.
+		// See http://blog.semanticart.com/blog/2013/11/11/a-proper-api-proxy-written-in-go/.
+		req.Host = req.URL.Host
+	}
+	return &httputil.ReverseProxy{Director: director}
 }
 
 func main() {
@@ -24,12 +60,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	cdnProxy := patchHost(httputil.NewSingleHostReverseProxy(cdnURL))
-	cdnProxyServer := httptest.NewServer(cdnProxy)
-	defer cdnProxyServer.Close()
+	trackingAPIURL, err := url.Parse("http://api.segment.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+	proxy := NewSegmentReverseProxy(cdnURL, trackingAPIURL)
+	proxyServer := httptest.NewServer(proxy)
+	defer proxyServer.Close()
 
 	// Test code.
-	url := cdnProxyServer.URL + "/v1/projects/DQf6nqU1PaMbcVQenYzYSRd6nkUL21b8/settings"
+	url := proxyServer.URL + "/v1/import/DQf6nqU1PaMbcVQenYzYSRd6nkUL21b8/settings"
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
