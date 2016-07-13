c5dc3026af81df77b9a421777de4b3fb014f4dcb
Prateek Srivastava <f2prateek@gmail.com>
2016-07-13 03:33:11 -0600

    adapt from http://blog.semanticart.com/blog/2013/11/11/a-proper-api-proxy-written-in-go/

--- main.go
+++ main.go
@@ -10,12 +10,21 @@ import (
 	"net/url"
 )
 
+// patchHost sets the host of the request to the host of of the destination URL.
+// See http://blog.semanticart.com/blog/2013/11/11/a-proper-api-proxy-written-in-go/.
+func patchHost(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Host = r.URL.Host
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	cdnURL, err := url.Parse("http://cdn.segment.com")
 	if err != nil {
 		log.Fatal(err)
 	}
-	cdnProxy := httputil.NewSingleHostReverseProxy(cdnURL)
+	cdnProxy := patchHost(httputil.NewSingleHostReverseProxy(cdnURL))
 	cdnProxyServer := httptest.NewServer(cdnProxy)
 	defer cdnProxyServer.Close()
 
