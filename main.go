bbe88c6b471e750ac159a7c28f13699cea938490
marten stromberg <marten@kive.ai>
2021-08-31 13:58:23 +0200

    allow next-integrations through proxy

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func NewSegmentReverseProxy(cdn *url.URL, trackingAPI *url.URL) http.Handler {
 	director := func(req *http.Request) {
 		// Figure out which server to redirect to based on the incoming request.
 		var target *url.URL
-		if strings.HasPrefix(req.URL.String(), "/v1/projects") || strings.HasPrefix(req.URL.String(), "/analytics.js/v1") || strings.HasPrefix(req.URL.String(), "/analytics-next/bundles") {
+		if strings.HasPrefix(req.URL.String(), "/v1/projects") || strings.HasPrefix(req.URL.String(), "/analytics.js/v1") || strings.HasPrefix(req.URL.String(), "/analytics-next/bundles") || strings.HasPrefix(req.URL.String(), "/next-integrations/") {if strings.HasPrefix(req.URL.String(), "/v1/projects") || strings.HasPrefix(req.URL.String(), "/analytics.js/v1") || strings.HasPrefix(req.URL.String(), "/analytics-next/bundles") || strings.HasPrefix(req.URL.String(), "/next-integrations/") {
 			target = cdn
 		} else {
 			target = trackingAPI
